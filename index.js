b1a7f38005b43fe3cad087cf22b4e55b2c1bbb60
unknown <luqmanmohdyazid@gmail.com>
2021-12-17 17:47:11 +0800

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, ()=>{ console.log(`Server is listening to port ${port} at http://localhost:${port}`)})
 
 //connect to mongodb
-mongoose.connect(process.env.DB_URI);
+mongoose.connect("mongodb://localhost:27017/Reservations");
 
 const database = mongoose.connection;
 
